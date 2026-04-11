bbfa61e3b15e432b7c1774221e55e93b06964877
Nixarr <nixarrow1@gmail.com>
2026-04-11 18:05:17 +0200

    Lexer fully implemented, Tokenizer

--- src/Scanner.java
+++ src/Scanner.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Scanner {
     private final String source;
@@ -7,6 +9,28 @@ public class Scanner {
     private int start = 0;
     private int current = 0;
     private int line = 1;
+
+    private static final Map<String, TokenType> keywords;
+
+    static {
+        keywords = new HashMap<>();
+        keywords.put("and",    TokenType.AND);
+        keywords.put("class",  TokenType.CLASS);
+        keywords.put("else",   TokenType.ELSE);
+        keywords.put("false",  TokenType.FALSE);
+        keywords.put("for",    TokenType.FOR);
+        keywords.put("fun",    TokenType.FUN);
+        keywords.put("if",     TokenType.IF);
+        keywords.put("nil",    TokenType.NIL);
+        keywords.put("or",     TokenType.OR);
+        keywords.put("print",  TokenType.PRINT);
+        keywords.put("return", TokenType.RETURN);
+        keywords.put("super",  TokenType.SUPER);
+        keywords.put("this",   TokenType.THIS);
+        keywords.put("true",   TokenType.TRUE);
+        keywords.put("var",    TokenType.VAR);
+        keywords.put("while",  TokenType.WHILE);
+    }
 
     Scanner(String source){
         this.source = source;
@@ -61,10 +85,84 @@ public class Scanner {
             case '\n':
                 line++;
                 break;
+            case '"':
+                string();
+                break;
             default:
-                NyxInt.error(line, "Unexpected character.");
+                if(isDigit(c)){
+                    number();
+                } else if (isAlpha(c)) {
+                    identifier();
+                } else {
+                    NyxInt.error(line, "Unexpected character.");
+                }
                 break;
         }
+    }
+
+    private void identifier() {
+        while (isAlphaNumeric(peek()))
+            advance();
+
+        String text = source.substring(start, current);
+        TokenType type = keywords.get(text);
+        if(type == null)
+            type = TokenType.IDENTIFIER;
+
+        addToken(type);
+    }
+
+    private boolean isAlpha(char c) {
+        return (c >= 'a' && c <= 'z') ||
+                (c >= 'A' && c <= 'Z') ||
+                c == '_';
+    }
+
+    private boolean isAlphaNumeric(char c) {
+        return isAlpha(c) || isDigit(c);
+    }
+
+    private boolean isDigit(char c){
+        return c >= '0' && c <= '9';
+    }
+
+    private void number() {
+        while (isDigit(peek()))
+            advance();
+
+        if(peek() == '.' && isDigit(peekNext())) {
+            advance();
+
+
+            while (isDigit(peek()))
+                advance();
+        }
+
+        addToken(TokenType.NUMBER, Double.parseDouble(source.substring(start, current)));
+
+    }
+
+    private char peekNext() {
+        if(current + 1 >= source.length())
+            return '\0';
+
+        return source.charAt(current+1);
+    }
+
+    private void string() {
+        while (peek() != '"' && ! isAtEnd()) {
+            advance();
+        }
+
+        if(isAtEnd()){
+            NyxInt.error(line, "Unterminated string");
+            return;
+        }
+
+        advance();
+
+        String value = source.substring(start + 1, current -1);
+        addToken(TokenType.STRING, value);
     }
 
     private boolean match(char expected){
